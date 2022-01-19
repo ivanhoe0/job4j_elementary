2cbd2adb573a6b67d3ca589ae6aa6282c248c90d
ivanhoe <corsair954@mail.ru>
2022-01-19 09:42:55 +0300

    4.1. Разбор первой программы. [#159033 #252372]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
 public class StudentInfo {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Ivanhoe");
         System.out.println("12.01.2022");
     }
