923150374e20a8c35d09de519322ad33f75d2b82
ivanhoe <corsair954@mail.ru>
2022-02-20 15:40:26 +0300

    6.9. Жадный алгоритм. Сдача в кофе машине [#34741 #261657]

--- src/main/java/ru/job4j/array/EndsWith.java
+++ src/main/java/ru/job4j/array/EndsWith.java
@@ -2,13 +2,13 @@ package ru.job4j.array;
 
 public class EndsWith {
     public static boolean endsWith(char[] word, char[] post) {
-        boolean result = true;
+        boolean rsl = true;
         for (int i = 0; i < post.length; i++) {
             if (post[post.length - 1 - i] != word[word.length - 1 - i]) {
-                result = false;
+                rsl = false;
                 break;
             }
         }
-        return result;
+        return rsl;
     }
 }
